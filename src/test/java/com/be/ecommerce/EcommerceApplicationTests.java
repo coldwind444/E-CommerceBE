331a527a9f9ca0b42071c6f0f3c84c8a76a4bc58
coldwind444 <minhtan14042004@gmail.com>
2024-12-07 21:46:51 +0700

    init

---
+++ src/test/java/com/be/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.be.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
